86d5b40309de7913c9f1862c9cb8152a82952031
CORA <sudeep2code@gmail.com>
2025-09-26 00:22:26 +0530

    Update qr_decode.py

--- qr_decode.py
+++ qr_decode.py
@@ -1,6 +1,6 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
 
-img = Image.open("C:/python/QRCODE_PY/samma.jpg")
+img = Image.open("C:/python/QRCODE_PY/samma.jpg") #Enter your path
 result = decode(img)
 print(result)
