1d81b494b4833e64060c17bd5528d3800b2f549f
CORA <sudeep2code@gmail.com>
2025-09-26 00:23:21 +0530

    Update qr_encode.py

--- qr_encode.py
+++ qr_encode.py
@@ -1,5 +1,5 @@
 import qrcode
 
-my_data = "google.com"
+my_data = "google.com" #give url
 img = qrcode.make(my_data)
-img.save("C:/python/QRCODE_PY/qrcode2.png")
+img.save("C:/python/QRCODE_PY/qrcode2.png") #enter path
